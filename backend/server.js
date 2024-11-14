b9af7a1a4df9976320174f03579baa166ff73998
sriramgandrothu <sriramgandrothu@gmail.com>
2024-11-14 22:03:19 +0530

    Test-Vercel

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ const corsOptions = {
   credentials: true,  // Allow credentials such as cookies, authorization headers, etc.
   origin: [
     'http://localhost:5173'
+    'https://eceresourcehub.vercel.app/'
   ],
 };
 app.use(cors(corsOptions));
